33ee423378a059b5effdbcac13d056b527dafcae
hancorg-spb <t13.spb@gmail.com>
2018-12-25 21:00:34 +0300

    eslint installation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const sayHello = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return console.log(`Hello ${userName}!`);
+const sayHello = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello ${userName}!`);
 };
+
+export default sayHello;
