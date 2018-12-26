5c5ffab570a4cc5911dc709bd648d5527cb251d2
hancorg-spb <t13.spb@gmail.com>
2018-12-26 17:49:33 +0300

    step 4 corrections

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 import readlineSync from 'readline-sync';
 
+const numberOfQuestions = 3;
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const isEven = number => number % 2 === 0;
+
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hello ${userName}!`);
 };
 
-
 const isEvenNumber = () => {
-  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!\n`);
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
-    const randomNum = randomNumber(1, 99);
-    console.log(`Question: ${randomNum}`);
+  for (let i = 0; i < numberOfQuestions; i += 1) {
+    const question = randomNumber(1, 99);
+    console.log(`Question: ${question}`);
     const isEvenQuestion = readlineSync.question('Your answer: ');
-    let trueAnswer = '';
-
-    if (randomNum % 2 === 0) {
-      trueAnswer = 'yes';
-    } else {
-      trueAnswer = 'no';
-    }
+    const trueAnswer = isEven(question) ? 'yes' : 'no';
 
     if (isEvenQuestion !== trueAnswer) {
-      return console.log(`'${isEvenQuestion}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again ${userName}!`);
+      console.log(`'${isEvenQuestion}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\n`);
+      return console.log(`Let's try again ${userName}!`);
     }
     console.log('Correct!');
   }
-  return console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
+  return null;
 };
 
 export { isEvenNumber, sayHello };
