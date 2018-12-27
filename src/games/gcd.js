17f8fde81d829029c1c2eb93397876d6d3584f8f
hancorg-spb <t13.spb@gmail.com>
2018-12-27 19:22:39 +0300

    step 6

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import { randomNumber, gameSample } from '..';
+
+const searchAnswer = (divisorOne, divisorTwo) => {
+  for (let i = Math.min(divisorOne, divisorTwo); i > 1; i -= 1) {
+    if (divisorOne % i === 0 && divisorTwo % i === 0) {
+      return i;
+    }
+  }
+  return 1;
+};
+
+const rule = 'Find the greatest common divisor of given numbers\n';
+
+const gcd = () => {
+  const numberOne = randomNumber(1, 99);
+  const numberTwo = randomNumber(1, 99);
+  const trueAnswer = String(searchAnswer(numberOne, numberTwo));
+  const question = `${numberOne} ${numberTwo}`;
+  return [question, trueAnswer];
+};
+
+export default () => gameSample(rule, gcd);
